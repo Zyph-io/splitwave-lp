1c94deb2fd1032bf4b6ce712ec41c8d99b5645fb
Vitor Alves <orvituhgo@gmail.com>
2024-07-16 20:51:52 -0300

    fix: update rd form

--- src/components/Sections/FormSection/FormSection.tsx
+++ src/components/Sections/FormSection/FormSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function FormSection() {
   const ref = useRef(null);
@@ -7,32 +7,22 @@ export default function FormSection() {
     //@ts-expect-error RDStation ira vim da biblioteca externa.
     if (window.RDStationForms && [...ref.current.children].length < 1) {
       //@ts-expect-error RDStation ira vim da biblioteca externa.
-      new window.RDStationForms('leads-b125aa7c11be23b21ed9', 'null').createForm()
-
+      new window.RDStationForms('digital-growth-c69546b557d81d6cfb85', 'null').createForm()
     }
-    
+
+    console.log('rendered')
   }, [])
 
   return (
-
     <>
       <div className="bg-blue-900">
         <div>
-          <h2 className="text-[40px]  font-normal font-ubuntu text-white  text-center">
+          <h2 className="text-[40px] font-normal font-ubuntu text-white  text-center">
             Pagamentos simples, inovadores e totalmente <br />personalizados à sua marca
           </h2>
         </div>
-        <div>
-          <div>
-           
-            <div role="main" id="leads-b125aa7c11be23b21ed9" ref={ref}></div>
-          </div>
-
-        </div>
+        <div className="max-w-[400px] mx-auto" role="main" id="digital-growth-c69546b557d81d6cfb85" ref={ref}></div>
       </div>
-
-
-
     </>
   )
 }
