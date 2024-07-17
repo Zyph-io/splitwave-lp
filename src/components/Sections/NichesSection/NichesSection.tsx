cc61f0b9c39573cc69740b73267da933a1bcb480
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2024-07-17 09:33:00 -0300

    Ajustado a versão do mobile

--- src/components/Sections/NichesSection/NichesSection.tsx
+++ src/components/Sections/NichesSection/NichesSection.tsx
@@ -8,7 +8,7 @@ import ecommerce from "../../../assets/ecommerce.svg";
 const NichesSection: React.FC = () => {
   return (
     <section className="bg-blue-900 text-white py-16 px-8">
-      <div className="max-w-7xl mx-auto flex justify-center items-center  text-center mb-12 ">
+      <div className="max-w-7xl mx-auto flex justify-center items-center  text-center mb-12 max-md:flex-col">
         <h2 className="text-3xl font-bold mt-[-7px] mr-2">PRINCIPAIS NICHOS DE ATUAÇÃO DA</h2>
           <div className='flex flex-col justify-center items-center'>
              <span className="text-blue-400  text-[38px] font-bold">SPLITWAVE</span>
