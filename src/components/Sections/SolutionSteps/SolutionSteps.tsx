a2ad592ab4abf36e31ac45cb5a68002450e0c601
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2024-07-17 11:10:52 -0300

    Ajuste na versão mobile na seção

--- src/components/Sections/SolutionSteps/SolutionSteps.tsx
+++ src/components/Sections/SolutionSteps/SolutionSteps.tsx
@@ -6,17 +6,17 @@ import traco from "../../../assets/traco.png"
 const SolutionSteps = () => {
   return (
     <section className="bg-blue-900 pt-28 pb-56 relative ">
-      <div className="container mx-auto px-4 text-center  text-white relative">
-        <div className='flex justify-center gap-1'>
+      <div className="container mx-auto px-4 text-center  text-white relative ">
+        <div className='flex justify-center gap-1 max-md:flex-col '>
           <div className=''>
-            <h2 className="font-inter font-bold text-[38px] ">O QUE A</h2>
+            <h2 className="font-inter font-bold text-[38px] max-md:text-[28px] ">O QUE A</h2>
           </div>
-          <div >
-            <h2 className="font-inter text-[38px] font-bold  "><span className="text-blue-300">SPLITWAVE</span></h2>
-            <img src={traco} alt="splitware" className='relative ml-[1.5rem]' />
+          <div className='max-md:flex max-md:flex-col max-md:items-center'>
+            <h2 className="font-inter text-[38px] font-bold  max-md:text-[28px]"><span className="text-blue-300">SPLITWAVE</span></h2>
+            <img src={traco} alt="splitware" className='relative ml-[1.5rem] max-md:w-40' />
           </div>
           <div>
-            <h2 className="font-inter text-[38px] font-bold ">TRAZ DE SOLUÇÃO?</h2>
+            <h2 className="font-inter text-[38px] font-bold max-md:text-[28px]">TRAZ DE SOLUÇÃO?</h2>
           </div>
         </div>
       </div>
--- src/components/common/LineRight/LineRight.tsx
+++ src/components/common/LineRight/LineRight.tsx
@@ -1,28 +1,28 @@
 export default function LineRight() {
     return (
         <>
-            <div className=" relative mt-56">
-                <div className="absolute border-t-[1px]  w-[75%] right-0 ">
-                    <div className=" mt-[-12px] ml-[-9px]  flex z-10 items-center justify-center w-6 h-6 border-2 border-blue-500 rounded-full">
+            <div className=" relative mt-56 max-md:mt-36">
+                <div className="absolute border-t-[1px]  w-[75%] right-0  max-md:w-[85%]">
+                    <div className=" mt-[-12px] ml-[-9px]  flex z-10 items-center justify-center w-6 h-6 border-2 border-blue-500 rounded-full max-md:ml-[-1] ">
                         <div className="absolute w-2 h-2 z-50   bg-[#3DA1DD] rounded-full"></div>
-                        <div className="absolute w-[198px] mt-[3rem] ml-[11rem]">
-                            <h4 className="font-inter text-white text-lg font-normal mt-24">
+                        <div className="absolute w-[198px] mt-[3rem] ml-[11rem] max-md:w-[98px] max-md:mt-[4rem] max-md:ml-16" >
+                            <h4 className="font-inter text-white text-lg font-normal mt-24 max-md:text-base  max-md:text-[13px] max-md:leading-[20px] max-md:pr-2">
                             Integração com múltiplas plataformas de e-commerce
                             </h4>
                         </div>
                     </div>
-                    <div className=" absolute mt-[-1.5rem] right-[65.6%] flex z-10 items-center justify-center w-6 h-6 border-2 border-blue-500 rounded-full">
+                    <div className=" absolute mt-[-1.5rem] right-[65.6%] flex z-10 items-center justify-center w-6 h-6 border-2 border-blue-500 rounded-full max-md:right-[50%]">
                         <div className="absolute w-2 h-2 z-50   bg-[#3DA1DD] rounded-full"></div>
-                        <div className="absolute w-[198px] mt-[3rem] ml-[11rem]">
-                            <h4 className="font-inter text-white text-lg font-normal mt-24">
+                        <div className="absolute max-w-[198px] mt-[3rem] ml-[11rem] max-md:mt-[1rem] max-md:ml-9">
+                            <h4 className="font-inter text-white text-lg font-normal mt-24 max-md:text-base   max-md:text-[13px] max-md:leading-[20px] max-md:pr-2" >
                             Relatórios <br /> detalhados
                             </h4>
                         </div>
                     </div>
-                    <div className=" absolute mt-[-1.5rem] right-[32.5%] flex z-10 items-center justify-center w-6 h-6 border-2 border-blue-500 rounded-full">
+                    <div className=" absolute mt-[-1.5rem] right-[32.5%] flex z-10 items-center justify-center w-6 h-6 border-2 border-blue-500 rounded-full max-md:right-0">
                         <div className="absolute w-2 h-2 z-50   bg-[#3DA1DD] rounded-full"></div>
-                        <div className="absolute w-[198px] mt-[5rem] ml-[11rem]">
-                            <h4 className="font-inter text-white text-lg font-normal mt-24">
+                        <div className="absolute max-w-[198px] mt-[5rem] ml-[11rem] max-md:mr-60 max-md:">
+                            <h4 className="font-inter text-white text-lg font-normal mt-24 max-md:text-[13px] max-md:leading-[20px] max-md:text-right max-md:pr-2">
                                 Checkout <br /> Personalizado e com alta taxa de aprovacão
                             </h4>
                         </div>
