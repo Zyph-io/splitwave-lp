8cbaa0c9612c06639bbc1dc35ec09847a7b8b151
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2024-07-17 09:30:57 -0300

    Ajustado a versão mobile da seção

--- src/components/Sections/PaymentFlowSection/PaymentFlowSection.tsx
+++ src/components/Sections/PaymentFlowSection/PaymentFlowSection.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import fluxo from "../../../assets/fluxo.svg";
 const PaymentFlowSection: React.FC = () => {
   return (
-    <section className="bg-blue-900 text-white pb-[229px] ">
+    <section className="bg-blue-900 text-white pb-[229px]  max-md:pb-20">
       <div className="flex flex-col justify-center items-center text-center mb-12">
         <h2 className="text-[38px] font-bold  font-inter">FLUXO DE SPLIT DE PAGAMENTOS FACILITADO</h2>
-        <p className="text-base w-[55rem] font-light font-poppins text-[#3DA1DD]">
+        <p className="text-base max-w-[55rem] font-light font-poppins text-[#3DA1DD] max-md:px-2">
           Comissione todas as recebedores envolvidos em um único fluxo financeiro.
            Adicione isso à nossa conciliação, antecipação de recebíveis e controle tudo através de uma só plataforma.
         </p>
       </div>
       <div className="flex justify-center items-center">
-        <div className="relative w-full max-w-6xl p-8 ">
+        <div className="relative max-w-full p-8  max-md:p-3">
           <img
             src={fluxo}
             alt="Fluxo de Split de Pagamentos"
-            className="shadow-md w-full rounded-[64px]"
+            className="shadow-md max-w-full rounded-[64px] max-md:rounded-md"
           />
         </div>
       </div>
