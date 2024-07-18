1488bf8c5f22155d76f3266b71609129c55bb181
Vitor Alves <orvituhgo@gmail.com>
2024-07-18 08:42:27 -0300

    fix(payment flow): set overflow-x when mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,6 @@ module.exports = {
          'buttom-color': "linear-gradient(90deg, #3DA1DD 50%, #6AC7F9 100%)",
          'buttom-servide': "linear-gradient(270deg, rgba(37, 104, 239, 0.5) 0%, rgba(34, 34, 34, 0.5) 100%)",
          'buttom-servide-plus': "linear-gradient(135deg, #2492FF 8.58%, #0043AE 91.42%)"
-        
-         
-         
-       
-
       },
       fontFamily:{
         ubuntu:['Ubuntu'],
