b3301d4334c1247d6bd489059a44debe6d234518
Vitor Alves <orvituhgo@gmail.com>
2024-08-06 19:54:03 -0300

    feat: add testimonials

--- src/components/common/CardComent/CardComent.tsx
+++ src/components/common/CardComent/CardComent.tsx
@@ -1,23 +1,53 @@
-import Profile from "../../../assets/Profile.svg";
-import stars from "../../../assets/stars.svg";
-import verification from "../../../assets/verication.svg"
-interface Props{
-    text: string;
+interface Props {
+  data: CardCommentData;
 }
 
-export default function CardComent({text}:Props) {
-    return(
-        <>
-          <div className=" max-w-[530px] bg-[#222222] flex flex-col justify-center items-center p-10 mx-auto rounded-xl  max-md:mx-2 max-md:p-5 ">
-             <div className="">
-              <img src={Profile} alt="Usuário" className="max-w-[59px] max-md:w-[30px]" />
-              <div className="flex my-6 max-md:my-1">
-                <img src={stars} alt="" className="max-w-[101px]  mr-1 max-md:w-[50px]" />
-                <img src={verification} alt="" className="max-w-[14px]" />
-              </div>
-             <p className="font-inter font-bold text-base max-md:text-xs ">{text}</p>
-             </div>
-          </div>        
-        </>
-    )
+interface CardCommentData {
+  title: string;
+  text: string;
+  stars: string;
+  profile: string;
+  verification: string;
+}
+
+export default function CardComent({ data }: Props) {
+  return (
+    <>
+      <div className="max-w-[530px] bg-[#222222] flex flex-col justify-center items-center p-10 mx-auto rounded-xl  max-md:mx-2 max-md:p-5">
+        <div className="">
+          {data.profile && (
+            <img
+              src={data.profile}
+              alt="Usuário"
+              className="max-w-[59px] max-md:w-[30px]"
+            />
+          )}
+          {(data.verification || data.stars) && (
+            <div className="flex my-6 max-md:my-1">
+              {data.stars && (
+                <img
+                  src={data.stars}
+                  alt=""
+                  className="max-w-[101px]  mr-1 max-md:w-[50px]"
+                />
+              )}
+              {data.verification && (
+                <img src={data.verification} alt="" className="max-w-[14px]" />
+              )}
+            </div>
+          )}
+          {data.title && (
+            <p className="font-inter font-bold text-base max-md:text-xs">
+              {data.title}
+            </p>
+          )}
+          {data.text && (
+            <p className="font-inter font-semibold text-base max-md:text-xs">
+              {data.text}
+            </p>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/common/Slider/Slider.tsx
+++ src/components/common/Slider/Slider.tsx
@@ -1,29 +1,77 @@
-import {register} from 'swiper/element/bundle';
-import {Swiper, SwiperSlide} from 'swiper/react'
-import CardComent from '../CardComent/CardComent';
-
-
-
+import { register } from "swiper/element/bundle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import CardComent from "../CardComent/CardComent";
 register();
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-
+const metadata = [
+  {
+    text: "O sistema da Splitwave está muito intuitivo. Com uma interface limpa, estamos conseguindo utilizá-lo sem grandes dificuldades. Nossos clientes também têm nos passado um feedback positivo sobre a utilização por eles. A constante atualização e o suporte eficaz também têm nos ajudado muito.",
+    title: "CentraPay",
+    stars: "",
+    profile: "",
+    verification: "",
+  },
+  {
+    text: "O sistema da Splitwave está muito intuitivo. Com uma interface limpa, estamos conseguindo utilizá-lo sem grandes dificuldades. Nossos clientes também têm nos passado um feedback positivo sobre a utilização por eles. A constante atualização e o suporte eficaz também têm nos ajudado muito.",
+    title: "CentraPay",
+    stars: "",
+    profile: "",
+    verification: "",
+  },
+  {
+    text: "O sistema da Splitwave está muito intuitivo. Com uma interface limpa, estamos conseguindo utilizá-lo sem grandes dificuldades. Nossos clientes também têm nos passado um feedback positivo sobre a utilização por eles. A constante atualização e o suporte eficaz também têm nos ajudado muito.",
+    title: "CentraPay",
+    stars: "",
+    profile: "",
+    verification: "",
+  },
+  {
+    text: "O sistema da Splitwave está muito intuitivo. Com uma interface limpa, estamos conseguindo utilizá-lo sem grandes dificuldades. Nossos clientes também têm nos passado um feedback positivo sobre a utilização por eles. A constante atualização e o suporte eficaz também têm nos ajudado muito.",
+    title: "CentraPay",
+    stars: "",
+    profile: "",
+    verification: "",
+  },
+  {
+    text: "O sistema da Splitwave está muito intuitivo. Com uma interface limpa, estamos conseguindo utilizá-lo sem grandes dificuldades. Nossos clientes também têm nos passado um feedback positivo sobre a utilização por eles. A constante atualização e o suporte eficaz também têm nos ajudado muito.",
+    title: "CentraPay",
+    stars: "",
+    profile: "",
+    verification: "",
+  },
+  {
+    text: "O sistema da Splitwave está muito intuitivo. Com uma interface limpa, estamos conseguindo utilizá-lo sem grandes dificuldades. Nossos clientes também têm nos passado um feedback positivo sobre a utilização por eles. A constante atualização e o suporte eficaz também têm nos ajudado muito.",
+    title: "CentraPay",
+    stars: "",
+    profile: "",
+    verification: "",
+  },
+];
 
 export default function Slider() {
-    
-    return (
-        <>
-            <Swiper pagination={true} autoplay={{ delay: 5000 }} slidesPerView={2} className=" absolute py-10 top-40 max-w-[60%]  max-md:max-w-[90%] max-md:mx-10 max-md:top-12 ">
-                <SwiperSlide> <CardComent text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget quam mauris.' /> </SwiperSlide>
-                <SwiperSlide> <CardComent text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget quam mauris.' /> </SwiperSlide>
-                <SwiperSlide> <CardComent text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget quam mauris.' /> </SwiperSlide>
-                <SwiperSlide> <CardComent text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget quam mauris.' /> </SwiperSlide>
-
-            </Swiper>
-
-        </>
-    )
+  return (
+    <>
+      {/* <Swiper pagination={true} autoplay={{ delay: 5000 }} slidesPerView={2} className=" absolute py-10 top-40 max-w-[60%]  max-md:max-w-[90%] max-md:mx-10 max-md:top-12 ">
+                {metadata.length && metadata.map(cardData => (
+                    <SwiperSlide>
+                        <CardComent data={cardData} />
+                    </SwiperSlide>
+                ))}
+            </Swiper> */}
+      <Swiper
+        pagination={true}
+        autoplay={{ delay: 5000 }}
+        slidesPerView={1}
+        className=" absolute py-10 top-40 w-full max-w-[60%]  max-md:max-w-[90%] max-md:mx-10 max-md:top-12 justify-center flex "
+      >
+        <SwiperSlide>
+          <CardComent data={metadata[0]} />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
